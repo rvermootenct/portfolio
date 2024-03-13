70989a96db62aef1fa675a465300139beafb57f4
Ryan <ryanvermootenbackup@gmail.com>
2024-03-13 16:11:52 +0200

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,23 @@ import About from "@/components/About"
 import Skills from "@/components/Skills";
 import WorkHistory from "@/components/WorkHistory";
 import Projects from "@/components/Projects";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="max-w-screen-xl w-full mx-auto px-4">
-      <div className="mt-20">
-        <Header />
-        <ExternalLinks />
-        <About />
-        <Skills />
-        <WorkHistory />
-        <Projects />
-      </div>
-   </main>
+    <>
+      <main className="max-w-screen-xl w-full mx-auto px-4">
+        <div className="mt-20">
+          <Header />
+          <ExternalLinks />
+          <About />
+          <Skills />
+          <WorkHistory />
+          <Projects />
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
  
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-t-zinc-100 dark:border-t-zinc-800 mt-8">
+      <div className="max-w-screen-xl mx-auto px-4 py-6">
+        <p className="text-sm font-semibold text-center text-zinc-800 dark:text-zinc-400">Made with Next.js & tailwind. Deployed using vercel</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
