96fcfb483e97f3d611c5306f666cc7732d6015f2
Ryan <ryanvermootenbackup@gmail.com>
2024-03-18 15:50:43 +0200

    style: cleanup styling of pdf

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -33,38 +33,38 @@ function SkillComponent({ skill }: {
 }) {
   return (
     <View style={tw('flex items-center space-x-2 bg-zinc-100 px-3 py-2')}>
-      <View style={tw('relative w-[25px] h-[25px]')}>
+      <View style={tw('relative w-[15px] h-[15px]')}>
         <Image src={getImagePath(skill)} style={tw('w-full h-full object-cover fill')} />
       </View>
-      <Text style={tw('text-sm md:text-base capitalize text-zinc-700 font-bold')}>{skill}</Text>
+      <Text style={tw('text-sm capitalize text-zinc-700 font-bold')}>{skill}</Text>
     </View>
   )
 }
 
 function Header() {
   return (
-    <View style={tw('flex flex-wrap flex-row items-center gap-y-9 gap-x-8 pb-7')}>
-      <View style={tw('w-[200px] h-[200px] relative rounded-xl overflow-hidden')}>
+    <View style={tw('flex flex-wrap flex-row items-center gap-x-8 pb-7')}>
+      <View style={tw('w-[75px] h-[75px] relative rounded-xl overflow-hidden')}>
         <Image
           src={getImagePath(MISC.PROFILE)}
           style={tw('w-full h-full object-cover')}
         />
       </View>
-      <View style={tw('flex-1')}>
-        <Text style={tw('text-5xl font-black capitalize text-black')}>
+      <View>
+        <Text style={tw('text-xl font-black capitalize text-black')}>
           Ryan Vermooten
         </Text>
-        <Text style={tw('mt-2 text-xl font-semibold capitalize text-zinc-400')}>
+        <Text style={tw('text-l font-semibold capitalize text-zinc-400')}>
           Senior full stack developer
         </Text>
         <View style={tw('flex flex-col gap-y-1 gap-x-4 mt-2')}>
           <View style={tw('flex space-x-2')}>
-            <Text style={tw('capitalize text-sm md:text-base font-semibold text-zinc-900')}>
+            <Text style={tw('capitalize text-sm font-semibold text-zinc-900')}>
               Cape Town, South Africa
             </Text>
           </View>
           <View style={tw('flex space-x-2')}>
-            <Text style={tw('capitalize text-sm md:text-base font-semibold text-zinc-900')}>
+            <Text style={tw('capitalize text-sm font-semibold text-zinc-900')}>
               7+ years experience
             </Text>
           </View>
@@ -76,7 +76,7 @@ function Header() {
 
 function Heading({ text }: { text: string }) {
   return (
-    <Text style={tw('uppercase text-2xl text-zinc-400 font-bold')}>
+    <Text style={tw('uppercase text-xl text-zinc-400 font-bold')}>
       {text}
     </Text>
   )
@@ -85,9 +85,9 @@ function Heading({ text }: { text: string }) {
 function Tablet({ info }: { info: Data}) {
   return (
     <View style={tw('border border-zinc-100 rounded-lg p-5 mb-6 mt-2')} wrap={false}>
-      <View style={tw('flex flex-wrap gap-5 w-full lg:w-8/12 mb-10')}>
+      <View style={tw('flex flex-wrap gap-5 w-full lg:w-8/12 mb-2')}>
         <View style={tw('flex flex-row gap-2')}>
-          <View style={tw('relative w-full md:w-[120px] pt-[56.25%] md:pt-0 md:h-20 rounded-lg overflow-hidden')}>
+          <View style={tw('relative w-full md:w-[60px] md:h-20 rounded-lg overflow-hidden')}>
             <View style={tw('w-full h-full absolute top-0 left-0 bottom-0 right-0')}>
               <Image
                 src={getImagePath(info.image)}
@@ -110,7 +110,7 @@ function Tablet({ info }: { info: Data}) {
           </View>
         </View>
       </View>
-      <View style={tw('flex flex-row gap-2 flex-wrap my-4')}>
+      <View style={tw('flex flex-row gap-2 flex-wrap my-2')}>
         {
               info.skills.map(
                 (skill) => (
@@ -119,7 +119,7 @@ function Tablet({ info }: { info: Data}) {
               )
             }
       </View>
-      <View style={tw('mt-6')}>
+      <View>
         {info.blurb.map((text) => (
           <Text key={text} style={tw('text-zinc-700 text-base font-semibold')}>{text}</Text>
         ))}
@@ -185,6 +185,10 @@ const MyDoc = (
         <Header />
         <About />
         <Skills />
+      </View>
+    </Page>
+    <Page size="A4">
+      <View style={tw('max-w-screen-xl w-full mx-auto p-4')}>
         <WorkHistory />
         <Projects />
       </View>
